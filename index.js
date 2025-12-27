28ea40a71301d6b8e9f5c631955ae13cbf2faaba
LOUIS NKAN ENOBONG <98494528+louisnkan@users.noreply.github.com>
2025-12-27 09:11:29 +0100

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.post('/api/gemini', async (req, res) => {
 
         console.log('Calling Gemini API');
 
-        const model = genAI.getGenerativeModel({ model: 'gemini-pro' });
+        const model = genAI.getGenerativeModel({ model: 'models/gemini-1.5-flash' });
         const result = await model.generateContent(prompt);
         const response = await result.response;
         const text = response.text();
